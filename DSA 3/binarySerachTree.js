2151df6576585939e96426ab57396c6bd4887123
Arjun <arjunmb1176@gmail.com>
2025-08-20 12:06:18 +0530

    binary search tree updated

--- DSA 3/binarySerachTree.js
+++ DSA 3/binarySerachTree.js
@@ -13,35 +13,33 @@ class BST{
     
     insert(value){
         const newNode = new Node(value)
-    
-        if(!this.root) {
+        if(!this.root){
             this.root = newNode
             return
-        }
-        
-        let curr = this.root
-        
-        while(true){
-            if(value < curr.value){
-                if(!curr.left){
-                    curr.left = newNode
-                    return
-                }
-                curr = curr.left
-            }else if(value > curr.value){
-                if(!curr.right){
-                    curr.right = newNode
-                    return
-                }
-                curr = curr.right
-            }else{
-                return
-            }
-        }
+         }
+         
+         let curr = this.root
+         while(true){
+             if(value < curr.value){
+                 if(!curr.left){
+                     curr.left = newNode
+                     return
+                 }
+                 curr = curr.left
+             }else if(value > curr.value){
+                 if(!curr.right){
+                     curr.right = newNode
+                     return 
+                 }
+                 curr = curr.right
+             }else{
+                 return
+             }
+         }
     }
     
     bfs(){
-        const res =[] ,queue = []
+        let queue = [], res = []
         
         if(this.root) queue.push(this.root)
         
@@ -54,7 +52,7 @@ class BST{
         return res
     }
     
-    inorder(node = this.root , res= []){
+    inorder(node = this.root, res = []){
         if(!node) return res
         this.inorder(node.left,res)
         res.push(node.value)
@@ -62,7 +60,7 @@ class BST{
         return res
     }
     
-    preorder(node = this.root, res = []){
+    preorder(node = this.root,res = []){
         if(!node) return res
         res.push(node.value)
         this.preorder(node.left,res)
@@ -70,7 +68,7 @@ class BST{
         return res
     }
     
-    postorder(node = this.root, res = []){
+    postorder(node = this.root,res= []){
         if(!node) return res
         this.postorder(node.left,res)
         this.postorder(node.right,res)
@@ -82,53 +80,61 @@ class BST{
         if(!node) return -1
         const left = this.height(node.left)
         const right = this.height(node.right)
-        
         return 1 + Math.max(left,right)
     }
-    
     depth(value){
         let curr = this.root
         let depth = 0
         while(curr){
-            if(curr.value === value){
-                return depth
-                
-            }
+            if(curr.value === value) return depth
             curr = value < curr.value ? curr.left : curr.right
             depth++
         }
         return -1
     }
     
-    kthLargest(v){
-        let count = 0
+    isBal(node= this.root){
+        if(!node) return true
+        const left  = this.height(node.left)
+        const right = this.height(node.right)
+        if(Math.abs(left- right) > 1) return false
+        return this.isBal(node.left) && this.isBal(node.right)
+    }
+    
+    isValidBST(node = this.root , min = -Infinity, max = -Infinity){
+        if(!node) return true
+        if(node.value <= min || node.value >= max) return false
+        return this.isValidBST(node.left,min,node.value) && this.isValidBST(node.right,node.value,max)
+    }
+    
+    kthLargest(k){
         let res = null
+        let count = 0
         
         const trav = (node) => {
-            if(!node || res !== null) return
+            if(!node || res !== null) return 
             trav(node.right)
-            count ++
-            if(count === v) res = node.value
+            count++
+            if(count === k) res= node.value
             trav(node.left)
         }
         trav(this.root)
         return res
     }
     
-    isBal(node = this.root){
-        if(!node) return true
-        const left = this.height(node.left)
-        const right = this.height(node.right)
+        kthSmallest(k){
+        let res = null
+        let count = 0
         
-        if(Math.abs(left - right) >  1) return false
-        return this.isBal(node.left) && this.isBal(node.right)
-    }
-    
-    isValid( node = this.root, min = -Infinity,max =Infinity){
-        if(!node) return true
-        if(node.value <= min || node.value >= max ) return false
-        
-        return this.isValid(node.left,min,node.value) && this.isValid(node.right,node.value,max)
+        const trav = (node) => {
+            if(!node || res !== null) return 
+            trav(node.left)
+            count++
+            if(count === k) res = node.value
+            trav(node.right)
+        }
+        trav(this.root)
+        return res
     }
     
     search(value){
@@ -143,53 +149,104 @@ class BST{
         return false
     }
     
-    delete(val){
-        let del =(node,val) => {
+    delete(value){
+        let del = (node,val) => {
             if(val < node.value){
                 node.left = del(node.left,val)
-            }else if(val> node.value){
+            }else if(val > node.value){
                 node.right = del(node.right,val)
             }else{
                 if(!node.left && !node.right) return null
-                if(!node.left) return node.right
                 if(!node.right) return node.left
+                if(!node.left) return node.right
                 
-                let min = this._finMin(node.right)
+                let min = this._findMin(node.right)
                 node.value = min.value
-                node.right = del(node.right,min.value)
+                node.right = del(node.right, min.value)
             }
-            return node
         }
-        this.root = del(this.root,val)
+        del(this.root,value)
     }
     
-    _finMin(node){
+    _findMin(node){
         while(node.left){
             node = node.left
         }
-    return node
+        return node
+    }
+    
+    findMin(node = this.root){
+        if(!node) return null
+        while(node.left) node = node.left
+        return node.value
+    }
+    
+    findMax(node = this.root){
+        if(!node) return null
+        while(node.right) node = node.right
+        return node.value
+    }
+    
+    countNode(node = this.root){
+        if(!node) return 0
+        return 1 + this.countNode(node.left) + this.countNode(node.right)
+    }
+    
+    countLeaves(node = this.root){
+        if(!node) return 0
+        if(!node.left && !node.right) return 1
+        return this.countLeaves(node.left) + this.countLeaves(node.right) 
+    }
+    
+    closestValue(target){
+        let curr = this.root
+        let closest = curr.value
+        
+        while(curr){
+            if(Math.abs(curr.value - target) < Math.abs(closest - target)) closest = curr.value
+            curr = target < curr.value ? curr.left : curr.right
+        }
+        return closest
     }
 }
 
+let bst = new BST();
+
+let values = [50, 30, 70, 20, 40, 60, 80];
+
+for (let v of values) {
+    bst.insert(v);
+}
+
+console.log("BFS    :- ", bst.bfs())
+console.log("INORDER   :- ",bst.inorder())
+console.log("PREORDER   :- ",bst.preorder())
+console.log("POSTORDER   :- ",bst.postorder())
+console.log("HEIGHT    :- ", bst.height())
+console.log("DEPTH    :- ", bst.depth(60))
+console.log("2nd Largest    :- ", bst.kthLargest(2))
+console.log("2nd SMALLEST      :- ", bst.kthSmallest(2))
+console.log("ISBALANCED    :- ", bst.isBal())
+console.log("ISVALID    :- ", bst.isValidBST())
+console.log("Search 5    :- ", bst.search(5))
+console.log("FINDMIN    :- ", bst.findMin())
+console.log("FINDMAX    :- ", bst.findMax())
+console.log("Count Number of Node    :- ", bst.countNode())
+console.log("Count Number of Leaves    :- ", bst.countLeaves())
+console.log("Closest value to 23    :- ", bst.closestValue())
 
 
-const bst = new BST()
-bst.insert(30)
-bst.insert(22)
-bst.insert(12)
-bst.insert(55)
-bst.insert(23)
-bst.insert(34)
 
-console.log("BSF : ", bst.bfs())
-console.log("inorder : ", bst.inorder())
-console.log("preorder : ", bst.preorder())
-console.log("postorder : ", bst.postorder())
-console.log("Is Valid BST : ", bst.isValid())
-console.log("height : ", bst.height())
-console.log("depth of 55 : ", bst.depth(55))
-console.log("1st largest : ",  bst.kthLargest(1))
-console.log("IsBalanced : ", bst.isBal())
-console.log("Search 55 : ", bst.search(12))
-bst.delete(12);
-console.log("Inorder after delete 12:", bst.inorder());
+
+
+
+
+
+
+
+
+
+
+
+
+
